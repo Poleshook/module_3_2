0a2cd81bb7098daf51dfa8e12dc3a74739a72bd5
Poleshook <poleshook@mail.ru>
2024-08-17 18:35:34 +0500

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,6 +1,6 @@
-def send_email(message, recipient, sender="university.help@gmail.com"):
-    a = ["@", ".com", ".ru", ".net"]
-    if not any(i in recipient for i in a) or not any(i in sender for i in a):
+def send_email(message,recipient,sender = "univerity.help@gmail.com"):
+    if ("@" not in recipient or not recipient.endswith((".com", ".ru", ".net"))
+            or "@" not in sender or not sender.endswith((".com", ".ru", ".net"))):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender == recipient:
         print("Нельзя отправить письмо самому себе!")
@@ -10,5 +10,4 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
 
 
-send_email("Привет", "Poleshookmail", "Poleshookmail.ru")
-
+send_email("Привет", "Poleshook@mail.net", "university.help@gmail.com")
